af67955cb26cfb984621b5e24a138d64844fd058
Veronica Kim <vero.a.kim@gmail.com>
2017-12-25 20:51:13 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=dog_name
+    @dog_name = dog_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=dog_breed
+    @dog_breed = dog_breed
+
+  def breed
+    @dog_name
+  end
+
+end
